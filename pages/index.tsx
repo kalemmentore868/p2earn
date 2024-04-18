af9e3a59b26ad1e929551d77519426204f3884ff
Kalem Mentore <kalem@Kalems-MacBook-Air.local>
2024-04-18 12:27:46 -0400

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,22 @@ import CreateGame from '@/components/CreateGame'
 import GameDetails from '@/components/GameDetails'
 import GameList from '@/components/GameList'
 import Hero from '@/components/Hero'
+import { globalActions } from '@/store/globalSlices'
 import { generateGameData } from '@/utils/fakeData'
 import { GameStruct } from '@/utils/type.dt'
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Page: NextPage<{ gamesData: GameStruct[] }> = ({ gamesData }) => {
+  const dispatch = useDispatch()
+  const { setGames } = globalActions
+
+  useEffect(() => {
+    dispatch(setGames(gamesData))
+  }, [dispatch, setGames, gamesData])
+
   return (
     <div>
       <Head>
